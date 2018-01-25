d5b7f6035770e034cb4670bb0aa2648bd2ce2cf1
PierreTchuente <33568610+PierreTchuente@users.noreply.github.com>
2018-01-25 08:59:42 +0200

    Update Model Ojective two
    Update Model Ojective two

--- MetricReport/app/models/center.rb
+++ MetricReport/app/models/center.rb
@@ -1,2 +1,4 @@
-class Center < ApplicationRecord
+class Center < ApplicationRecord::Base
+    attr_accessible :emis, :centerNumber, :school_id 
+    has_many :School
 end
--- MetricReport/app/models/school.rb
+++ MetricReport/app/models/school.rb
@@ -1,2 +1,4 @@
-class School < ApplicationRecord
+class School < ApplicationRecord::Base
+    attr_accessible :name, :numberWrote, :numberPass, :numberFailed, :year
+    belongs_to :Center
 end
